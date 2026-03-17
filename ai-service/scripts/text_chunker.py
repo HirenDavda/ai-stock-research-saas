ea57fdbf0d2fb3a8aaa634de84a0e70314b7d324
HIREN macbook air <hiren@HIRENs-MacBook-Air.local>
2026-03-17 14:01:44 +0530

    Day 3: Implemented document chunking system

---
+++ ai-service/scripts/text_chunker.py
@@ -0,0 +1,46 @@
+# Import the text splitter from LangChain
+# This tool helps break large text into smaller pieces
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# Import our PDF loader function
+from document_loader import load_pdf
+
+# Function to split text into chunks
+def split_chunk_text_into_chunks(text):
+
+    # Create a text splitter object
+    # chunk_size = maximum characters in one chunk
+    # chunk_overlap = small overlap between chunks to keep context
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200
+    )
+
+    # Split the full text into smaller chunks
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+if __name__ == "__main__":
+
+    # Path to your annual report
+    pdf_file_path = "../data/dodla_dairy_annual_report_2025.pdf"
+
+    # Load text from the PDF
+    document_text = load_pdf(pdf_file_path)
+
+    # Split the text into chunks
+    document_chunks = split_chunk_text_into_chunks(document_text)
+
+    # Print number of chunks created
+    print(f"Total chunks created: {len(document_chunks)}")
+
+    # Print first chunk for testing
+    print("\nFirst Chunk:\n")
+    print(document_chunks[0])
+
+    # Save chunks to a text file for inspection
+    with open("chunks_output.txt", "w") as f:
+        for chunk in document_chunks:
+            f.write(chunk)
+            f.write("\n---\n")  # Separator between chunks  
